f8cb63b57e37c1eae2a6b94ad66525af170e16da
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2025-10-24 09:50:01 -0400

    feat: crear o escribir archivos

--- screenMatch/src/com/aluracursos/screenmatch/principal/MainWithSearch.java
+++ screenMatch/src/com/aluracursos/screenmatch/principal/MainWithSearch.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -49,6 +50,11 @@ public class MainWithSearch {
 
             Title myTitle = new Title(myTitleOmdb);
             System.out.println("Titulo convertido: " + myTitle);
+
+            FileWriter fileWrite = new FileWriter("Peliculas.txt");
+            fileWrite.write(myTitle.toString());
+            fileWrite.close();
+
         }catch (NumberFormatException e) {
             System.out.println("Ocurrio un error!");
             System.out.println(e.getMessage());
